707c5a103de640d1f628dbe89578be8a4856a64e
TheVisuals <127110583+ifnino@users.noreply.github.com>
2024-07-17 12:42:54 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from "react";
 import { Container, Col, Row } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg";
+import mine1 from "../assets/img/images (777).png"
 
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
-    const toRotate = ["web Developer", "web Designer", "UI/UX Designer", "Full-stack web developer", "Data Analyst"];
+    const toRotate = ["A web Developer", "A web Designer", "A UI/UX Designer", "A Full-stack web developer", "Data Analyst"];
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300 - Math.random() * 100);
     const period = 2000;
@@ -48,14 +49,14 @@ export const Banner = () => {
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to my Portfolio</span>
                         <h1 className="purple-gold-text">
-                            {`Hi I'm Bipin `}
+                            {`Hi I'm Bipin, `}
                             <span className="wrap">{text}</span>
                         </h1>
                         <p>Hey there, this is Bipin, a full-stack web developer</p>
                         <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25} /></button>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
-                        <img src={headerImg} alt="Header Img" />
+                        <img src={mine1} alt="Header Img" />
                     </Col>
                 </Row>
             </Container>
